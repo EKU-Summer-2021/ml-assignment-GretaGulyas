3cbe82b1110e7eb261eeadf65b0c3ca058a0b605
Gulyás Gréta <gulyas.greta@yahoo.com>
2021-07-14 09:32:08 +0200

    feat: implemented train-test split

--- main.py
+++ main.py
@@ -4,5 +4,6 @@
 from src.strength_prediction_with_lr import ReadConcreteStrength
 
 if __name__ == '__main__':
-    concrete_strength = ReadConcreteStrength(r'datasets/Concrete_Data_Yeh.csv')
-    print(concrete_strength.concrete_strength_dataset_read())
+    cs = ReadConcreteStrength(r'datasets/Concrete_Data_Yeh.csv')
+    print(cs.concrete_strength_dataset_read())
+    print(cs.train_test_split())
--- src/strength_prediction_with_lr.py
+++ src/strength_prediction_with_lr.py
@@ -5,18 +5,30 @@
 # pylint: disable=too-few-public-methods
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 class ReadConcreteStrength:
     """
         Class with method that reads the concrete strength dataset.
     """
-    def __init__(self, concrete_strength):
-        self.concrete_strength = concrete_strength
+
+    def __init__(self, dataset_location):
+        self.dataset = dataset_location
+        self.concrete_strength_dataset = self.concrete_strength_dataset_read()
 
     def concrete_strength_dataset_read(self):
         """
             Function for reading the CSV file.
         """
-        return pd.read_csv(self.concrete_strength, names=['cement', 'slag', 'flyash', 'water', 'superplasticizer',
-                                                             'coarseaggregate', 'fineaggregate', 'age', 'csMPa'])
+        return pd.read_csv(self.dataset, names=['cement', 'slag', 'flyash', 'water', 'superplasticizer',
+                                                'coarseaggregate', 'fineaggregate', 'age', 'csMPa'])
+
+    def train_test_split(self):
+        """
+            Function for train-test split.
+        """
+
+        training_data, testing_data = train_test_split(self.concrete_strength_dataset, test_size=0.2, random_state=25)
+        print('Train', training_data.shape)
+        print('Test', testing_data.shape)
