bd08e93bede5229a2831305fa3242b169ae30f18
Gulyás Gréta <gulyas.greta@yahoo.com>
2021-07-21 13:58:25 +0200

    fix: test fixed

--- tests/linear_regression_test.py
+++ tests/linear_regression_test.py
@@ -63,7 +63,7 @@ class LinearRegressionTest(unittest.TestCase):
         # when
         lre.grid_search(param_grid, x_train, y_train)
         lre.save_result(x_test, y_test)
-        print(os.getcwd())
+        print(os.listdir())
         ACTUAL = os.path.isdir('../Results')
         # then
         self.assertEqual(EXPECTED, ACTUAL)
