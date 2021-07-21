f0283f579d9c446cbe844689c9dd66d8bfec9c9d
Gulyás Gréta <gulyas.greta@yahoo.com>
2021-07-21 13:47:57 +0200

    fix: new test added to check the save method

--- tests/linear_regression_test.py
+++ tests/linear_regression_test.py
@@ -3,6 +3,7 @@
 """
 
 import unittest
+import os
 from src.linear_regression import LRExample
 from src.concrete_strength_dataset_read import ReadDataset
 from sklearn.linear_model import LinearRegression
@@ -13,7 +14,7 @@ class LinearRegressionTest(unittest.TestCase):
         Linear regression test method.
     """
 
-    def test_read(self):
+    def test_dataset_read(self):
         """
             Linear regression testing.
         """
@@ -31,7 +32,7 @@ class LinearRegressionTest(unittest.TestCase):
         # then
         self.assertEqual(EXPECTED, ACTUAL)
 
-    def test_score(self):
+    def test_model_score(self):
         cs = ReadDataset('https://raw.githubusercontent.com/EKU-Summer-2021/ml-assignment-GretaGulyas/master/datasets/Concrete_Data_Yeh.csv')
         lre = LRExample('https://raw.githubusercontent.com/EKU-Summer-2021/ml-assignment-GretaGulyas/master/datasets/Concrete_Data_Yeh.csv')
         x_train, x_test, y_train, y_test = lre.split_data(cs)
@@ -46,3 +47,22 @@ class LinearRegressionTest(unittest.TestCase):
         ACTUAL = lre.search.best_score_
         # then
         self.assertGreater(ACTUAL, EXPECTED)
+
+    def test_save_result(self):
+        cs = ReadDataset(
+            'https://raw.githubusercontent.com/EKU-Summer-2021/ml-assignment-GretaGulyas/master/datasets/Concrete_Data_Yeh.csv')
+        lre = LRExample(
+            'https://raw.githubusercontent.com/EKU-Summer-2021/ml-assignment-GretaGulyas/master/datasets/Concrete_Data_Yeh.csv')
+        x_train, x_test, y_train, y_test = lre.split_data(cs)
+        param_grid = [
+            {'fit_intercept': [True, False],
+                'normalize': [True, False]}
+        ]
+        # given
+        EXPECTED = True
+        # when
+        lre.grid_search(param_grid, x_train, y_train)
+        lre.save_result(x_test, y_test)
+        ACTUAL = os.path.isdir('../Results')
+        # then
+        self.assertEqual(EXPECTED, ACTUAL)
